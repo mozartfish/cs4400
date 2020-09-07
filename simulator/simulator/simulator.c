0016f8de91ba92b6509b8eaf06dbc9c89af6a42c
Pranav Rajan <rajan.pranav320@gmail.com>
2020-09-07 12:20:35 -0700

    initialize memory to 0

--- simulator/simulator/simulator.c
+++ simulator/simulator/simulator.c
@@ -23,79 +23,81 @@
 
 // Forward declarations for helper functions
 unsigned int get_file_size(int file_descriptor);
-unsigned int* load_file(int file_descriptor, unsigned int size);
-instruction_t* decode_instructions(unsigned int* bytes, unsigned int num_instructions);
-unsigned int execute_instruction(unsigned int program_counter, instruction_t* instructions, 
-				 int* registers, unsigned char* memory);
-void print_instructions(instruction_t* instructions, unsigned int num_instructions);
-void error_exit(const char* message);
+unsigned int *load_file(int file_descriptor, unsigned int size);
+instruction_t *decode_instructions(unsigned int *bytes, unsigned int num_instructions);
+unsigned int execute_instruction(unsigned int program_counter, instruction_t *instructions,
+                                 int *registers, unsigned char *memory);
+void print_instructions(instruction_t *instructions, unsigned int num_instructions);
+void error_exit(const char *message);
 
 // 17 registers
 #define NUM_REGS 17
 // 1024-byte stack
 #define STACK_SIZE 1024
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   // Make sure we have enough arguments
-  if(argc < 2)
+  if (argc < 2)
     error_exit("must provide an argument specifying a binary file to execute");
 
   // Open the binary file
   int file_descriptor = open(argv[1], O_RDONLY);
-  if (file_descriptor == -1) 
+  if (file_descriptor == -1)
     error_exit("unable to open input file");
 
   // Get the size of the file
   unsigned int file_size = get_file_size(file_descriptor);
   // Make sure the file size is a multiple of 4 bytes
   // since machine code instructions are 4 bytes each
-  if(file_size % 4 != 0)
+  if (file_size % 4 != 0)
     error_exit("invalid input file");
 
   // Load the file into memory
   // We use an unsigned int array to represent the raw bytes
   // We could use any 4-byte integer type
-  unsigned int* instruction_bytes = load_file(file_descriptor, file_size);
+  unsigned int *instruction_bytes = load_file(file_descriptor, file_size);
   close(file_descriptor);
 
   unsigned int num_instructions = file_size / 4;
-
 
   /****************************************/
   /**** Begin code to modify/implement ****/
   /****************************************/
 
   // Allocate and decode instructions (left for you to fill in)
-  instruction_t* instructions = decode_instructions(instruction_bytes, num_instructions);
+  instruction_t *instructions = decode_instructions(instruction_bytes, num_instructions);
 
   // Optionally print the decoded instructions for debugging
   // Will not work until you implement decode_instructions
   // Do not call this function in your submitted final version
   // print_instructions(instructions, num_instructions);
 
-
   // Once you have completed Part 1 (decoding instructions), uncomment the below block
 
-
   // Allocate and initialize registers
-  int* registers = (int*)malloc(sizeof(int)*NUM_REGS);
+  int *registers = (int *)malloc(sizeof(int) * NUM_REGS);
   // TODO: initialize register values+
 
   // Stack memory is byte-addressed, so it must be a 1-byte type
   // TODO allocate the stack memory. Do not assign to NULL.
-  unsigned char* memory = (unsigned char*)malloc(sizeof(char)*STACK_SIZE);
+  unsigned char *memory = (unsigned char *)malloc(sizeof(char) * STACK_SIZE);
+  int j;
+  for (j = 0; j < STACK_SIZE; j++)
+  {
+    memory[j] = 0;
+  }
 
   // Run the simulation
   unsigned int program_counter = 0;
 
-  // program_counter is a byte address, so we must multiply num_instructions by 4 
+  // program_counter is a byte address, so we must multiply num_instructions by 4
   // to get the address past the last instruction
-  while(program_counter != num_instructions * 4)
+  while (program_counter != num_instructions * 4)
   {
     program_counter = execute_instruction(program_counter, instructions, registers, memory);
   }
-  
+
   return 0;
 }
 
@@ -103,40 +105,39 @@ int main(int argc, char** argv)
  * Decodes the array of raw instruction bytes into an array of instruction_t
  * Each raw instruction is encoded as a 4-byte unsigned int
 */
-instruction_t* decode_instructions(unsigned int* bytes, unsigned int num_instructions)
+instruction_t *decode_instructions(unsigned int *bytes, unsigned int num_instructions)
 {
   // TODO: Don't return NULL
   // instruction_t* retval = NULL;
-  instruction_t* retval = (instruction_t*)malloc(num_instructions*sizeof(instruction_t));
-  
+  instruction_t *retval = (instruction_t *)malloc(num_instructions * sizeof(instruction_t));
+
   /*
   int i;
   for(i = ...){
     retval[i] = (fill in fields based on raw bits);
   */
   int i;
-  for (i = 0; i < num_instructions; i++) {
-    retval[i].opcode = bytes[i] >> 27;     
+  for (i = 0; i < num_instructions; i++)
+  {
+    retval[i].opcode = bytes[i] >> 27;
     retval[i].first_register = (bytes[i] >> 22) & 0x1F;
     retval[i].second_register = (bytes[i] >> 17) & 0x1F;
     retval[i].immediate = bytes[i] & 0xFFFF;
   }
-    
+
   return retval;
-
 }
-
 
 /*
  * Executes a single instruction and returns the next program counter
 */
-unsigned int execute_instruction(unsigned int program_counter, instruction_t* instructions, int* registers, unsigned char* memory)
+unsigned int execute_instruction(unsigned int program_counter, instruction_t *instructions, int *registers, unsigned char *memory)
 {
   // program_counter is a byte address, but instructions are 4 bytes each
   // divide by 4 to get the index into the instructions array
   instruction_t instr = instructions[program_counter / 4];
-  
-  switch(instr.opcode)
+
+  switch (instr.opcode)
   {
   case subl:
     registers[instr.first_register] = registers[instr.first_register] - instr.immediate;
@@ -151,9 +152,7 @@ unsigned int execute_instruction(unsigned int program_counter, instruction_t* in
     scanf("%d", &(registers[instr.first_register]));
     break;
 
-
-  // TODO: Implement remaining instructions
-
+    // TODO: Implement remaining instructions
   }
 
   // TODO: Do not always return program_counter + 4
@@ -162,7 +161,6 @@ unsigned int execute_instruction(unsigned int program_counter, instruction_t* in
   // program_counter + 4 represents the subsequent instruction
   return program_counter + 4;
 }
-
 
 /*********************************************/
 /****  DO NOT MODIFY THE FUNCTIONS BELOW  ****/
@@ -181,15 +179,15 @@ unsigned int get_file_size(int file_descriptor)
 /*
  * Loads the raw bytes of a file into an array of 4-byte units
 */
-unsigned int* load_file(int file_descriptor, unsigned int size)
+unsigned int *load_file(int file_descriptor, unsigned int size)
 {
-  unsigned int* raw_instruction_bytes = (unsigned int*)malloc(size);
-  if(raw_instruction_bytes == NULL)
+  unsigned int *raw_instruction_bytes = (unsigned int *)malloc(size);
+  if (raw_instruction_bytes == NULL)
     error_exit("unable to allocate memory for instruction bytes (something went really wrong)");
 
   int num_read = read(file_descriptor, raw_instruction_bytes, size);
 
-  if(num_read != size)
+  if (num_read != size)
     error_exit("unable to read file (something went really wrong)");
 
   return raw_instruction_bytes;
@@ -199,17 +197,17 @@ unsigned int* load_file(int file_descriptor, unsigned int size)
  * Prints the opcode, register IDs, and immediate of every instruction, 
  * assuming they have been decoded into the instructions array
 */
-void print_instructions(instruction_t* instructions, unsigned int num_instructions)
+void print_instructions(instruction_t *instructions, unsigned int num_instructions)
 {
   printf("instructions: \n");
   unsigned int i;
-  for(i = 0; i < num_instructions; i++)
+  for (i = 0; i < num_instructions; i++)
   {
-    printf("op: %d, reg1: %d, reg2: %d, imm: %d\n", 
-	   instructions[i].opcode,
-	   instructions[i].first_register,
-	   instructions[i].second_register,
-	   instructions[i].immediate);
+    printf("op: %d, reg1: %d, reg2: %d, imm: %d\n",
+           instructions[i].opcode,
+           instructions[i].first_register,
+           instructions[i].second_register,
+           instructions[i].immediate);
   }
   printf("--------------\n");
 }
@@ -217,7 +215,7 @@ void print_instructions(instruction_t* instructions, unsigned int num_instructio
 /*
  * Prints an error and then exits the program with status 1
 */
-void error_exit(const char* message)
+void error_exit(const char *message)
 {
   printf("Error: %s\n", message);
   exit(1);
