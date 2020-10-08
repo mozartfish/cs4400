72081061f7f0bbcce46c8ce2b9eebae21e22479e
Pranav Rajan <rajan.pranav320@gmail.com>
2020-10-07 18:29:04 -0700

    added loop tiling for complex function

--- performance/perflab-handout-release/kernels.c
+++ performance/perflab-handout-release/kernels.c
@@ -63,11 +63,41 @@ void first_complex(int dim, pixel *src, pixel *dest)
     {
       pixel p = src[RIDX(i, j, dim)];
       int colorVal = (int)(p.red + p.green + p.blue) / 3;
-      int pIndex = (int)(RIDX(dim - j - 1, dim - i - 1, dim));
+      int pIndex = (RIDX(dim - j - 1, dim - i - 1, dim));
       dest[pIndex].red = colorVal;
       dest[pIndex].green = colorVal;
       dest[pIndex].blue = colorVal;
     }
+}
+char second_complex_descr[] = "second_complex: loop tiling";
+void second_complex(int dim, pixel *src, pixel *dest)
+{
+  int i, j, ii, jj, incr;
+
+  // increment value
+  incr = 32;
+
+  for (ii = 0; ii < dim; ii += incr)
+  {
+    int dimConst = dim - 1;
+    for (jj = 0; jj < dim; jj += incr)
+    {
+      for (i = ii; i < ii + incr; i++)
+      {
+        int di = dimConst - i;
+        for (j = jj; j < jj + incr; j++)
+        {
+          int dj = dimConst - j;
+          pixel p = src[RIDX(i, j, dim)];
+          int colorVal = (int)(p.red + p.green + p.blue) / 3;
+          int pIndex = (RIDX(dj, di, dim));
+          dest[pIndex].red = colorVal;
+          dest[pIndex].green = colorVal;
+          dest[pIndex].blue = colorVal;
+        }
+      }
+    }
+  }
 }
 
 /* 
@@ -78,7 +108,7 @@ char complex_descr[] = "complex: Current working version";
 void complex(int dim, pixel *src, pixel *dest)
 {
   // naive_complex(dim, src, dest);
-  first_complex(dim, src, dest);
+  second_complex(dim, src, dest);
 }
 
 /*********************************************************************
@@ -94,6 +124,7 @@ void register_complex_functions()
   add_complex_function(&complex, complex_descr);
   add_complex_function(&naive_complex, naive_complex_descr);
   add_complex_function(&first_complex, first_complex_descr);
+  add_complex_function(&second_complex, second_complex_descr);
 }
 
 /***************
