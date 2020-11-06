f82c54bc58355a90f2303757c3887fcc1ca03df4
Pranav Rajan <rajan.pranav320@gmail.com>
2020-11-06 08:30:17 -0800

    added in malloc

---
+++ malloc/malloclab-handout/mm.c
@@ -0,0 +1,71 @@
+/*
+ * mm-naive.c - The least memory-efficient malloc package.
+ * 
+ * In this naive approach, a block is allocated by allocating a
+ * new page as needed.  A block is pure payload. There are no headers or
+ * footers.  Blocks are never coalesced or reused.
+ *
+ * NOTE TO STUDENTS: Replace this header comment with your own header
+ * comment that gives a high level description of your solution.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+/* always use 16-byte alignment */
+#define ALIGNMENT 16
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1))
+
+/* rounds up to the nearest multiple of mem_pagesize() */
+#define PAGE_ALIGN(size) (((size) + (mem_pagesize()-1)) & ~(mem_pagesize()-1))
+
+void *current_avail = NULL;
+int current_avail_size = 0;
+
+/* 
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(void)
+{
+  current_avail = NULL;
+  current_avail_size = 0;
+  
+  return 0;
+}
+
+/* 
+ * mm_malloc - Allocate a block by using bytes from current_avail,
+ *     grabbing a new page if necessary.
+ */
+void *mm_malloc(size_t size)
+{
+  int newsize = ALIGN(size);
+  void *p;
+  
+  if (current_avail_size < newsize) {
+    current_avail_size = PAGE_ALIGN(newsize);
+    current_avail = mem_map(current_avail_size);
+    if (current_avail == NULL)
+      return NULL;
+  }
+
+  p = current_avail;
+  current_avail += newsize;
+  current_avail_size -= newsize;
+  
+  return p;
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void *ptr)
+{
+}
