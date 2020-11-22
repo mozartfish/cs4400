f72bc37dd4db4d992ea9a4522b58a6865bbc04a1
Pranav Rajan <rajan.pranav320@gmail.com>
2020-11-22 11:48:06 -0800

    found another bug

--- malloc/malloclab-handout/mm.c
+++ malloc/malloclab-handout/mm.c
@@ -167,8 +167,8 @@ void *mm_malloc(size_t size)
   // // variable that stores the aligned size of the amt of bytes requested by the user
   // int new_size = ALIGN(MAX (size, total_overhead));
 
-  // // pointer that will be returned to the user that points to a contiguous chunk of memory that fits the size requested by the user
-  // void *p;
+  // pointer that will be returned to the user that points to a contiguous chunk of memory that fits the size requested by the user
+  void *p;
 
   // // check if the heap is null 
   // // CASE 1: IF THE HEAP IS NULL ->  REQUEST MEMORY
@@ -206,7 +206,7 @@ void *mm_malloc(size_t size)
   // current_avail += new_size;
   // current_avail_size -= new_size;
 
-  // return p;
+  return p;
 }
 
 static void extend(size_t s) {
