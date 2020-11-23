05d1658714d51374d6e69eb3c9c7e4af7d4538aa
Pranav Rajan <rajan.pranav320@gmail.com>
2020-11-22 18:30:27 -0800

    finished writing the extend function ?

--- malloc/dummy/malloclab-handout/mm.c
+++ malloc/dummy/malloclab-handout/mm.c
@@ -35,10 +35,11 @@ typedef struct header_block {
   struct header_block *next; // pointer to the next header block
 } header_block;
 
-// 16 bytes
+// 24 bytes
 typedef struct footer_block {
   size_t block_size; // variable for indicating the size
   size_t allocated; // variable for indicating whether the block has been allocated
+  struct footer_block *next; // pointer to the next footer block 
 } footer_block;
 
 // 24 bytes
@@ -50,15 +51,16 @@ typedef struct page_node {
 /**********************************************************************************************/
 /* GLOBAL VARIABLES */
 static page_node *heap = NULL; // global variable for keeping track of the heap
-void *mem_pages = NULL; // global variable for keeping track of the pages in use 
-
-void *current_avail = NULL;
-int current_avail_size = 0;
+void *current_avail = NULL; // global variable for keeping of the memory returned by mem_map
+int current_avail_size = 0; // the size of the available pages
 /**********************************************************************************************/
 /* HELPER FUNCTIONS */
 static void extend(size_t s);
 static void add_pages(void *page_amt);
 /*********************************************************************************************/
+/*MACROS FOR DEFINING CONSTANTS */
+#define OVERHEAD (sizeof(page_node) + sizeof(header_block) + sizeof(footer_block) + sizeof(header_block) + sizeof(footer_block) + sizeof(header_block))
+/********************************************************************************************/
 
 /* 
  * mm_init - initialize the malloc package.
@@ -68,10 +70,8 @@ mm_init(void)
 {
   // initialize structs
   heap = NULL;
-  mem_pages = NULL;
   current_avail = NULL;
   current_avail_size = 0;
-
   return 0;
 }
 
@@ -96,6 +96,83 @@ void *mm_malloc(size_t size)
   current_avail_size -= newsize;
   
   return p;
+}
+
+/* Request more memory by calling mem_map
+*  Initialize the new chunk of memory as applicable
+*  Update free list if applicable
+*/
+static void extend(size_t s) {
+  // first align the pages
+  current_avail_size = PAGE_ALIGN(s);
+  current_avail = mem_map(current_avail_size); // currently a void * pointer
+
+  // update the page linked list
+  add_pages(current_avail);
+
+  // cast current available to a char*
+  char *page_struct = (char *)current_avail;
+
+  // cast 24 bytes onwards as a block header for prologue header
+  header_block *prologue_header = (header_block *)(page_struct + 24);
+  prologue_header->block_size = 48;
+  prologue_header->allocated = 1;
+  prologue_header->next = NULL;
+
+  // cast the next 24 bytes onwards as a block footer for prologue footer
+  footer_block *prologue_footer = (footer_block *)(page_struct + 48);
+  prologue_footer->block_size = 48;
+  prologue_footer->allocated = 1;
+  prologue_footer->next = NULL;
+
+  // cast the next 24 bytes onward as a block header for the payload header
+  header_block *payload_header = (header_block *)(page_struct + 72);
+  payload_header->block_size = current_avail_size - OVERHEAD;
+  payload_header->allocated = 0;
+  payload_header->next = NULL;
+
+
+// cast the second to last 24 bytes as a block footer for the payload footer
+  footer_block *payload_footer = (footer_block *)(page_struct - 48);
+  payload_footer->block_size = current_avail_size - OVERHEAD;
+  payload_footer->allocated = 0;
+  payload_footer->next = NULL;
+  
+  // cast the last 24 bytes as header for the epilogue
+  header_block *epilogue = (header_block *)(page_struct - 24);
+  epilogue->block_size = 0;
+  epilogue->allocated = 1;
+  epilogue->next = NULL;
+
+  // // update the current available size
+  // current_avail_size -= sizeof(header_block);
+
+  // 24 bytes are reserved for linked list information
+  // 24 bytes reserved for the prologue bytes reserved for the prologue header
+  // 24 bytes reserved for the prologue footer
+  // 24 bytes reserved for the epilogue header
+}
+
+static void add_pages(void *page_amt) {
+  // convert the page amount to a node
+  page_node *new_page_chunk = (page_node *)page_amt;
+
+  // conver the page amount to char
+  char *new_page_chunk_byte = (char *)page_amt;
+
+  // CASE 1: HEAP IS NULL
+  if (heap == NULL) {
+    heap = new_page_chunk;
+    heap->next = NULL;
+    heap->prev = NULL;
+    heap->bytes_avail = new_page_chunk_byte - OVERHEAD;
+  }
+  else {
+    new_page_chunk->next = heap;
+    new_page_chunk->prev = NULL;
+    new_page_chunk->bytes_avail = new_page_chunk_byte - OVERHEAD;
+    heap = new_page_chunk;
+  }
 }
 
 /*
