b75a149bea150751ac9f7937be85ec1d88f43310
Pranav Rajan <rajan.pranav320@gmail.com>
2020-11-23 17:25:02 -0800

    rewriting malloc

--- malloc/dummy/malloclab-handout/mm.c
+++ malloc/dummy/malloclab-handout/mm.c
@@ -25,43 +25,66 @@
 
 /* rounds up to the nearest multiple of mem_pagesize() */
 #define PAGE_ALIGN(size) (((size) + (mem_pagesize()-1)) & ~(mem_pagesize()-1))
-
-/***************************************************************************************/
-/*DEFINE DATA STRUCTURES FOR MALLOC */
-// 24 bytes 
-typedef struct header_block {
-  size_t block_size; // variable for indicating the size
-  size_t allocated; // variable for indicating whether the block has been set
-  struct header_block *next; // pointer to the next header block
-} header_block;
-
-// 24 bytes
-typedef struct footer_block {
-  size_t block_size; // variable for indicating the size
-  size_t allocated; // variable for indicating whether the block has been allocated
-  struct footer_block *next; // pointer to the next footer block 
-} footer_block;
-
-// 24 bytes
-typedef struct page_node {
-  struct page_node *next; // pointer to the next set of pages in the memory list
-  struct page_node *prev; // pointer to the previous set of pages in the memory list
-  size_t bytes_avail; // variable for keeping track of the number of bytes used in a chunk of pages
-} page_node;
-/**********************************************************************************************/
-/* GLOBAL VARIABLES */
-static page_node *heap = NULL; // global variable for keeping track of the heap
-void *current_avail = NULL; // global variable for keeping of the memory returned by mem_map
-int current_avail_size = 0; // the size of the available pages
 /**********************************************************************************************/
 /* HELPER FUNCTIONS */
 static void extend(size_t s);
-static void add_pages(void *page_amt);
+static void add_to_free_list(void *bp);
+static void set_allocated(void *b, size_t size);
 /*********************************************************************************************/
-/*MACROS FOR DEFINING CONSTANTS */
-#define OVERHEAD (sizeof(page_node) + sizeof(header_block) + sizeof(footer_block) + sizeof(header_block) + sizeof(footer_block) + sizeof(header_block))
-/********************************************************************************************/
+/*MACRO CONSTANTS*/
+// This assumes you have a struct or typedef called "block_header" and "block_footer"
+#define OVERHEAD (sizeof(block_header)+sizeof(block_footer))
 
+// Given a payload pointer, get the header or footer pointer
+#define HDRP(bp) ((char *)(bp) - sizeof(block_header))
+#define FTRP(bp) ((char *)(bp)+GET_SIZE(HDRP(bp))-OVERHEAD)
+
+// Given a payload pointer, get the next or previous payload pointer
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE((char *)(bp)-OVERHEAD))
+
+// Given a pointer to a header, get or set its value
+#define GET(p) (*(size_t *)(p))
+#define PUT(p, val) (*(size_t *)(p) = (val))
+
+// Combine a size and alloc bit
+#define PACK(size, alloc) ((size) | (alloc))
+
+// Given a header pointer, get the alloc or size
+#define GET_ALLOC(p) (GET(p) & 0x1)
+#define GET_SIZE(p) (GET(p) & ~0xF)
+
+/********************************************************************************************/
+/*DEFINE DATA STRUCTURES FOR MALLOC */
+typedef size_t block_header;
+typedef size_t block_footer;
+
+// 16 bytes
+// typedef struct header_block
+// {
+//   size_t block_size; // variable for indicating the size
+//   size_t allocated;  // variable for indicating whether the block has been set
+// } block_header;
+
+// // 16 bytes
+// typedef struct footer_block
+// {
+//   size_t block_size; // variable for indicating the size
+//   size_t allocated;  // variable for indicating whether the block has been allocated
+// } block_footer;
+
+/*Doubly Linked List Data Structure for keeping track of the free blocks */
+typedef struct list_node
+{
+  struct list_node *prev;
+  struct list_node *next;
+} list_node;
+/*********************************************************************************************/
+/* GLOBAL VARIABLES */
+static list_node *free_list = NULL; // global variable for keeping track of the free blocks
+void *current_avail = NULL;         // global variable for keeping of the memory returned by mem_map
+int current_avail_size = 0;         // the size of the available pages
+/**********************************************************************************************/
 /* 
  * mm_init - initialize the malloc package.
  */
@@ -69,7 +92,7 @@ static void add_pages(void *page_amt);
 mm_init(void)
 {
   // initialize structs
-  heap = NULL;
+  free_list = NULL;
   current_avail = NULL;
   current_avail_size = 0;
   return 0;
@@ -83,19 +106,38 @@ void *mm_malloc(size_t size)
 {
   int newsize = ALIGN(size);
   void *p;
-  
-  if (current_avail_size < newsize) {
-    current_avail_size = PAGE_ALIGN(newsize);
-    current_avail = mem_map(current_avail_size);
-    if (current_avail == NULL)
-      return NULL;
-  }
 
-  p = current_avail;
-  current_avail += newsize;
-  current_avail_size -= newsize;
+  // // check if the heap is empty
+  // if (heap == NULL) {
+  //   extend(newsize);
+  // }
+
+  // page_chunk *current_page_chunk = heap;
+
+  // header_block *start = (header_block *) (current_page_chunk + sizeof(page_chunk) + sizeof(header_block) + sizeof(footer_block)); // 24 bytes * 3 = 72 -> start at byte 72
+  // header_block *end = (header_block *) (current_page_chunk + current_avail_size - sizeof(header_block));
+
+  // while (start < end) {
+  //   // if the size of the header is satisfies the request
+  //   if (GET_SIZE(start) >= newsize) {
+  //     // check if the block is allocated
+  //     if (GET_ALLOC(start) == 1) {
+  //       start
+  //     }
+  //   }
+  // }
+  // if (current_avail_size < newsize) {
+  //   current_avail_size = PAGE_ALIGN(newsize);
+  //   current_avail = mem_map(current_avail_size);
+  //   if (current_avail == NULL)
+  //     return NULL;
+  // }
+
+  // p = current_avail;
+  // current_avail += newsize;
+  // current_avail_size -= newsize;
   
-  return p;
+  // return p;
 }
 
 /* Request more memory by calling mem_map
@@ -103,46 +145,73 @@ void *mm_malloc(size_t size)
 *  Update free list if applicable
 */
 static void extend(size_t s) {
-  // first align the pages
+  // align the number of bytes for the pages requested
   current_avail_size = PAGE_ALIGN(s);
+
+  // request some memory
   current_avail = mem_map(current_avail_size); // currently a void * pointer
 
-  // update the page linked list
-  add_pages(current_avail);
+  // cast current avail to char* to pack the information for the 
+  // prologue header, prologue footer, payload header, payload footer, epilogue header
+  char *total_bytes = (char *)(current_avail);
 
-  // cast current available to a char*
-  char *page_struct = (char *)current_avail;
+  // Constants for the different parts of the page list chunk
+  char *prologue_header = total_bytes + 8;
+  char *prologue_footer = total_bytes + 16;
+  char *payload_header = total_bytes + 24;
+  char *payload_pointer = total_bytes + 32;
+  char *payload_footer = FTRP(payload_pointer);
+  char *epilogue_header = payload_footer + 8;
 
-  // cast 24 bytes onwards as a block header for prologue header
-  header_block *prologue_header = (header_block *)(page_struct + 24);
-  prologue_header->block_size = 48;
-  prologue_header->allocated = 1;
-  prologue_header->next = NULL;
+  // PROLOGUE is a 32-byte allocated block consisting of header and footer
+  PUT(prologue_header, PACK(16, 1));
+  PUT(prologue_footer, PACK(16, 1));
+  PUT(payload_header, PACK(current_avail_size - 32, 0));
+  PUT(payload_footer, PACK(current_avail_size - 32, 0));
+  PUT(payload_footer, PACK(0, 1));
 
-  // cast the next 24 bytes onwards as a block footer for prologue footer
-  footer_block *prologue_footer = (footer_block *)(page_struct + 48);
-  prologue_footer->block_size = 48;
-  prologue_footer->allocated = 1;
-  prologue_footer->next = NULL;
-
-  // cast the next 24 bytes onward as a block header for the payload header
-  header_block *payload_header = (header_block *)(page_struct + 72);
-  payload_header->block_size = current_avail_size - OVERHEAD;
-  payload_header->allocated = 0;
-  payload_header->next = NULL;
+  // add the node to the free block linked list 
 
 
-// cast the second to last 24 bytes as a block footer for the payload footer
-  footer_block *payload_footer = (footer_block *)(page_struct - 48);
-  payload_footer->block_size = current_avail_size - OVERHEAD;
-  payload_footer->allocated = 0;
-  payload_footer->next = NULL;
-  
-  // cast the last 24 bytes as header for the epilogue
-  header_block *epilogue = (header_block *)(page_struct - 24);
-  epilogue->block_size = 0;
-  epilogue->allocated = 1;
-  epilogue->next = NULL;
+  //   // typecast current available to set the number of bytes from page_align size
+  //   page_chunk *page_struct = (page_chunk *)(current_avail);
+  //   page_struct->bytes_avail = current_avail_size;
+
+  //   // update the page linked list
+  //   add_pages(current_avail);
+
+  //   // cast current available to a char*
+  //   // char *page_struct = (char *)current_avail;
+
+  //   // cast 24 bytes onwards as a block header for prologue header
+  //   header_block *prologue_header = (header_block *)(page_struct + sizeof(page_chunk));
+  //   prologue_header->block_size = 48;
+  //   prologue_header->allocated = 1;
+  //   prologue_header->next = NULL;
+
+  //   // cast the next 24 bytes onwards as a block footer for prologue footer
+  //   footer_block *prologue_footer = (footer_block *)(page_struct + sizeof(page_chunk) + sizeof(header_block));
+  //   prologue_footer->block_size = 48;
+  //   prologue_footer->allocated = 1;
+  //   prologue_footer->next = NULL;
+
+  //   // cast the next 24 bytes onward as a block header for the payload header
+  //   header_block *payload_header = (header_block *)(page_struct + sizeof(page_chunk) + sizeof(header_block) + sizeof(footer_block));
+  //   payload_header->block_size = current_avail_size - OVERHEAD;
+  //   payload_header->allocated = 0;
+  //   payload_header->next = NULL;
+
+  // // cast the second to last 24 bytes as a block footer for the payload footer
+  //   footer_block *payload_footer = (footer_block *)(current_avail_size - sizeof(header_block) - sizeof(footer_block));
+  //   payload_footer->block_size = current_avail_size - OVERHEAD;
+  //   payload_footer->allocated = 0;
+  //   payload_footer->next = NULL;
+
+  //   // cast the last 24 bytes as header for the epilogue
+  //   header_block *epilogue = (header_block *)(current_avail_size - sizeof(header_block));
+  //   epilogue->block_size = 0;
+  //   epilogue->allocated = 1;
+  //   epilogue->next = NULL;
 
   // // update the current available size
   // current_avail_size -= sizeof(header_block);
@@ -153,27 +222,55 @@ static void extend(size_t s) {
   // 24 bytes reserved for the epilogue header
 }
 
-static void add_pages(void *page_amt) {
+/*
+* Function that takes a block
+* and adds it to the free list
+*/
+static void add_to_free_list(void *bp) {
   // convert the page amount to a node
-  page_node *new_page_chunk = (page_node *)page_amt;
+  list_node *new_block = (list_node *)(bp);
 
-  // conver the page amount to char
-  char *new_page_chunk_byte = (char *)page_amt;
-
-  // CASE 1: HEAP IS NULL
-  if (heap == NULL) {
-    heap = new_page_chunk;
-    heap->next = NULL;
-    heap->prev = NULL;
-    heap->bytes_avail = new_page_chunk_byte - OVERHEAD;
+  // CASE 1: THE FREE LIST IS EMPTY
+  if (free_list == NULL) {
+    free_list = new_block;
+    free_list->next = NULL;
+    free_list->prev = NULL;
   }
   else {
-    new_page_chunk->next = heap;
-    new_page_chunk->prev = NULL;
-    new_page_chunk->bytes_avail = new_page_chunk_byte - OVERHEAD;
-    heap = new_page_chunk;
+    free_list->prev = new_block;
+    new_block->next = free_list;
+    new_block->prev = NULL;
+    free_list = new_block;
   }
 }
+
+// /* Set a block to allocated
+// *  Update block headers/footers as needed
+// *  Update free list if applicable
+// *  Split block if applicable
+// */
+// static void set_allocated(void *b, size_t size) {
+//   // cast void *b to header block to get old size
+//   header_block *current_header = (header_block *)(b);
+
+//   // get the current size in the header
+//   size_t old_size = current_header->block_size;
+
+//   // compute a new size
+//   size_t size_difference = old_size - size;
+
+// // check if we can split page 
+//   if (size_difference >= (sizeof(header_block) + sizeof(footer_block)))
+
+//   // update the current header size
+//   current_header->block_size = size_difference;
+//   // set the block to allocated
+//   current_header->allocated = 1;
+//   // create a new footer block
+//   footer_block *new_footer = (footer_block *)(b + sizeof(header_block) + size)
+//   // 
+
+// }
 
 /*
  * mm_free - Freeing a block does nothing.
