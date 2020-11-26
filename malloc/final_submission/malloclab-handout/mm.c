317423fecda440c3a6be8142f66bf0303d7d8227
Pranav Rajan <rajan.pranav320@gmail.com>
2020-11-25 20:34:19 -0800

    added stuff

--- malloc/final_submission/malloclab-handout/mm.c
+++ malloc/final_submission/malloclab-handout/mm.c
@@ -116,8 +116,8 @@ void *mm_malloc(size_t size)
     bp = first_bp;
     while (GET_SIZE(HDRP(bp)) != 0)
     {
-      printf("The size available is : %d", GET_SIZE(HDRP(bp)));
-      printf("The new size: %d", new_size);
+      // printf("The size available is : %d", GET_SIZE(HDRP(bp)));
+      // printf("The new size: %d", new_size);
       if (!GET_ALLOC(HDRP(bp)) && (GET_SIZE(HDRP(bp))) >= new_size)
       {
         set_allocated(bp, new_size);
@@ -210,8 +210,8 @@ static void set_allocated(void *bp, size_t asize)
     PUT(FTRP(bp), PACK(asize, 1));
     bp = NEXT_BLKP(bp);
     PUT(HDRP(bp), PACK(csize - asize, 0));
-    printf("next size %d\n", GET_SIZE(HDRP(bp)));
-    printf("next alloc %d\n", GET_ALLOC(HDRP(bp)));
+    // printf("next size %d\n", GET_SIZE(HDRP(bp)));
+    // printf("next alloc %d\n", GET_ALLOC(HDRP(bp)));
     PUT(FTRP(bp), PACK(csize - asize, 0));
   }
   else
