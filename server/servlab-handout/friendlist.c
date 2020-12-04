610e2377945ec0f821eb67540d23ee012d415918
Pranav Rajan <rajan.pranav320@gmail.com>
2020-12-03 18:25:57 -0800

    print some stuff

--- server/servlab-handout/friendlist.c
+++ server/servlab-handout/friendlist.c
@@ -513,24 +513,23 @@ static void serve_introduce(int fd, dictionary_t *query)
   int i;
   for (i = 0; friend_list[i] != NULL; ++i)
   {
-    printf("name: %s", friend_list[i]);
-    // add_friends(user, friend_list[i]);
+    add_friends(user, friend_list[i]);
   }
 
-  //   printf("check the dictionary\n");
-  //   char **users = dictionary_keys(user_dict);
-  //   int p;
-  //   int g;
-  //   for (p = 0; users[p] != NULL; ++p)
-  //   {
-  //     printf("name : %s\n", users[p]);
-  //     dictionary_t *friend_friends_new = (dictionary_t *)(dictionary_get(user_dict, users[p]));
-  //     char **friends_list = dictionary_keys(friend_friends_new);
-  //     for (g = 0; friends_list[g] != NULL; ++g)
-  //     {
-  //       printf("friend: %s\n", friends_list[g]);
-  //     }
-  //   }
+    printf("check the dictionary\n");
+    char **users = dictionary_keys(user_dict);
+    int p;
+    int g;
+    for (p = 0; users[p] != NULL; ++p)
+    {
+      printf("name : %s\n", users[p]);
+      dictionary_t *friend_friends_new = (dictionary_t *)(dictionary_get(user_dict, users[p]));
+      char **friends_list = dictionary_keys(friend_friends_new);
+      for (g = 0; friends_list[g] != NULL; ++g)
+      {
+        printf("friend: %s", friends_list[g]);
+      }
+    }
   printf("end server response\n");
 
   /* Read request line and headers */
