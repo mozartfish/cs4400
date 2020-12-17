fa18651c0a657fe233240dd5b105129211f618ad
Pranav Rajan <rajan.pranav320@gmail.com>
2020-12-16 22:38:23 -0800

    try some stuff

--- malloc/malloclab-handout/mm.c
+++ malloc/malloclab-handout/mm.c
@@ -120,7 +120,7 @@ void *mm_malloc(size_t size)
 
   if (free_list == NULL) {
     // call th extend function
-    extend(2 * new_size);
+    extend(128 * new_size);
   }
 
   list_node *current_free_block = free_list;
@@ -132,7 +132,7 @@ void *mm_malloc(size_t size)
     }
     else if (current_free_block->next == NULL) {
       // extend the new size
-      extend(2 * new_size);
+      extend(128 * new_size);
       current_free_block = free_list;
     }
     else {
