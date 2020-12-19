9f648ab5e9cdef15c703139b6ed5a6a48cd37cf9
Pranav Rajan <rajan.pranav320@gmail.com>
2020-12-18 19:31:45 -0800

    save stuff

--- malloc/malloclab-handout/mm.c
+++ malloc/malloclab-handout/mm.c
@@ -372,9 +372,9 @@ static void *coalesce(void *bp)
   else
   {
     printf("enter case 4\n");
+    size += (GET_SIZE(HDRP(prev_payload)) + GET_SIZE(HDRP(next_payload)));
     remove_from_free_list(prev_payload); // remove previous
     remove_from_free_list(next_payload); // remove next
-    size += (GET_SIZE(HDRP(prev_payload)) + GET_SIZE(HDRP(next_payload)));
     PUT(HDRP(prev_payload), PACK(size, 0));
     PUT(FTRP(next_payload), PACK(size, 0));
     add_to_free_list(prev_payload);
